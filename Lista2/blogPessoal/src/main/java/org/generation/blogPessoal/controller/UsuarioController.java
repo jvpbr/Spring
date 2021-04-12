7cd5b6c8c09e8d7a1b13a93e8108bd4b7bc42bc5
unknown <joseparanan@gmail.com>
2021-04-12 19:34:40 -0300

    #

--- Lista2/blogPessoal/src/main/java/org/generation/blogPessoal/controller/UsuarioController.java
+++ Lista2/blogPessoal/src/main/java/org/generation/blogPessoal/controller/UsuarioController.java
@@ -1,14 +1,18 @@
 package org.generation.blogPessoal.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.generation.blogPessoal.model.UserLogin;
 import org.generation.blogPessoal.model.Usuario;
+import org.generation.blogPessoal.repository.UsuarioRepository;
 import org.generation.blogPessoal.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +25,20 @@ public class UsuarioController {
 	
 	@Autowired
 	private UsuarioService usuarioService;
+	
+	@Autowired
+	private UsuarioRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Usuario>> getAll(){
+		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	@GetMapping("/{usuario}")
+	public ResponseEntity<Usuario> getByUsuario(@PathVariable String usuario){
+		return repository.findByUsuario(usuario).map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
+	}
 	
 	@PostMapping("/logar")
 	public ResponseEntity<UserLogin> Autentication(@RequestBody Optional<UserLogin> user) {
